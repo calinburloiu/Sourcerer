9752c2b41327ad7dc91f9184d29392fe500d9a0b
Calin-Andrei Burloiu <calin.burloiu@gmail.com>
2012-08-01 12:08:01 +0800

    testing: QueriesTest class updated to new DDB API

--- testing/src/edu/nus/soc/sourcerer/testing/ddb/QueriesTest.java
+++ testing/src/edu/nus/soc/sourcerer/testing/ddb/QueriesTest.java
@@ -11,6 +11,7 @@ import edu.nus.soc.sourcerer.ddb.HBaseConnectionException;
 import edu.nus.soc.sourcerer.ddb.HBaseException;
 import edu.nus.soc.sourcerer.ddb.queries.ProjectModelInserter;
 import edu.nus.soc.sourcerer.ddb.queries.ProjectsRetriever;
+import edu.nus.soc.sourcerer.ddb.util.ListModelAppender;
 import edu.nus.soc.sourcerer.model.ddb.ProjectModel;
 import edu.uci.ics.sourcerer.model.Project;
 
@@ -33,6 +34,7 @@ public class QueriesTest {
     ProjectsRetriever retriever;
     
     // Create a project model object to be inserted.
+   
     Project type = Project.CRAWLED;
     String name = "Test Project";
     String description = "Carpe diem! YOLO!";
@@ -43,9 +45,12 @@ public class QueriesTest {
     Boolean hasSource = true;
     Integer loc = 1000;
     Integer nwloc = 900;
-    inputProject = new ProjectModel(type, name, description, version, group, path,
-        hash, hasSource, loc, nwloc);
+  	
+  
     
+    inputProject = new ProjectModel(type.getValue(), name, description,
+        version, group, path, hash, hasSource, loc, nwloc);
+  
     models.add(inputProject);
     
     // Compute project ID.
@@ -58,7 +63,10 @@ public class QueriesTest {
       
       // Retrieve the same project.
       retriever = new ProjectsRetriever();
-      outputProject = retriever.retrieveProjects(inputProject.getType(), id);
+      ListModelAppender<ProjectModel> appender =
+          new ListModelAppender<ProjectModel>();
+      retriever.retrieveProjects(appender, inputProject.getType(), id);
+      outputProject = appender.getList().get(0);
     } catch (HBaseConnectionException e) {
       e.printStackTrace();
     } catch (HBaseException e) {
@@ -67,7 +75,13 @@ public class QueriesTest {
     
     // Check if the inserted project matches the retrieved project.
     assertEquals(inputProject.getName(), outputProject.getName());
-    // ...
+    assertEquals(inputProject.getDescription(), outputProject.getDescription());
+    assertEquals(inputProject.getVersion(), outputProject.getVersion());
+    assertEquals(inputProject.getGroup(), outputProject.getGroup());
+    assertEquals(inputProject.getPath(), outputProject.getPath());
+    assertEquals(inputProject.hasSource(), outputProject.hasSource());
+    assertEquals(inputProject.getLoc(), outputProject.getLoc());
+    assertEquals(inputProject.getNwloc(), outputProject.getNwloc());
     assertArrayEquals(inputProject.getHash(), outputProject.getHash());
   }
 }
